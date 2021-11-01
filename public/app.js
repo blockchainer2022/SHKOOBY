64b08221efc54960439cf4d42d11d7e5e460cd83
blockchainer2022 <=blockchainer.2202@gmail.com>
2021-11-01 19:38:04 +0530

    added scroll to top

--- public/app.js
+++ public/app.js
@@ -44,3 +44,27 @@ listItems.forEach((item) => {
 menubar.addEventListener("click", () => {
   mobileMenu.classList.toggle("active");
 });
+
+//scroll to top
+
+var mybutton = document.querySelector(".scroll-to-top");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+mybutton.addEventListener("click", topFunction);
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
